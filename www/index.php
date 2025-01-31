12504056a5c6f4c55c2bbe18c773275739d16f27
Adam <thomasadam83@hotmail.com>
2025-01-31 10:38:42 -0500

    Added LAMP yml file

---
+++ www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+
+    <h1>Home Page</h1>
+
+    <?php echo "<p>Hello Word!</p>"; ?>
+    
+</body>
+</html>
